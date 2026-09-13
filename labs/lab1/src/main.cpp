4d8c74054d6b719d21fb06ffa457263510b9e7b6
Max <trofimovm230@yandex.ru>
2026-09-13 18:24:37 +0400

    feat: added functions AddEnd and Erase

--- labs/lab1/src/main.cpp
+++ labs/lab1/src/main.cpp
@@ -5,6 +5,9 @@ struct Point
 {
     T _x = 0;
     T _y = 0;
+
+    Point() : _x(0), _y(0) {}
+    Point(T x, T y) : _x(x), _y(y) {}
 };
 
 template <typename T>
@@ -45,6 +48,29 @@ private:
     }
 
 public:
+    Polyline() : _data(nullptr), _size(0), _capacity(0) {}
+
+    ~Polyline() { delete[] _data; }
+
+    void AddEnd(T element)
+    {
+        if (_size == _capacity)
+        {
+            int new_capacity = (_capacity == 0) ? 1 : new_capacity * 2;
+            Reallocate(new_capacity, -1);
+        }
+
+        _data[_size] = element;
+        ++_size;
+    }
+
+    void Erase(int index)
+    {
+        int new_capacity = (_capacity == 0) ? throw std::logic_error("Error delete index") : new_capacity - 1;
+        Reallocate(new_capacity, index);
+
+        --_size;
+    }
 };
 
 int main()
